50cb2e1dfaad31852174ee975d9cf5017eed39d4
Andrew Dinh <andrewdinh74@yahoo.com>
2021-06-14 22:37:37 -0700

    Show Modal and Backdrop when a Todo is about to be deleted

--- react-refresher/src/components/Todo.js
+++ react-refresher/src/components/Todo.js
@@ -1,6 +1,12 @@
-function Todo(props) {
-  function deleteHandler() {
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
 
+function Todo(props) {
+  const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true)
   }
 
   return (
@@ -9,6 +15,8 @@ function Todo(props) {
       <div className="actions">
         <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
+      {modalIsOpen && <Modal/>}
+      {modalIsOpen && <Backdrop/>}
     </div>
   );
 }
