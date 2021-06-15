4c4508ea1b9da11e4dc565f5777da1991a2ea547
Andrew Dinh <andrewdinh74@yahoo.com>
2021-06-15 12:34:15 -0700

    Can close the backdrop

--- react-refresher/src/components/Todo.js
+++ react-refresher/src/components/Todo.js
@@ -9,6 +9,10 @@ function Todo(props) {
     setModalIsOpen(true)
   }
 
+  function closeModalHandler() {
+    setModalIsOpen(false)
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -16,7 +20,7 @@ function Todo(props) {
         <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
       {modalIsOpen && <Modal/>}
-      {modalIsOpen && <Backdrop/>}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler}/>}
     </div>
   );
 }
