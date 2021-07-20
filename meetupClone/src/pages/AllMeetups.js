a2039dcbd9028154ec364ecb7781b4eed813daef
Andrew Dinh <andrewdinh74@yahoo.com>
2021-07-19 23:43:49 -0700

    Able to use data from Firebase for meetups

--- meetupClone/src/pages/AllMeetups.js
+++ meetupClone/src/pages/AllMeetups.js
@@ -12,8 +12,18 @@ function AllMeetupsPage() {
     ).then((response) => {
       return response.json();
     }).then((data) => {
+      const meetups = [];
+
+      for (const key in data) {
+        const meetup = {
+          id: key,
+          ...data[key]
+        }
+
+        meetups.push(meetup);
+      }
       setIsLoading(false);
-      setLoadedMeetups(data);
+      setLoadedMeetups(meetups);
     })
   }, [])
 
