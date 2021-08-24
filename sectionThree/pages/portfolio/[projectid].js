ed383e6554f7b5aee726c9701e592a37db8671f0
Andrew Dinh <andrewdinh74@yahoo.com>
2021-08-23 21:41:56 -0700

    Use router query to extract projectid

--- sectionThree/pages/portfolio/[projectid].js
+++ sectionThree/pages/portfolio/[projectid].js
@@ -1,6 +1,11 @@
+import { useRouter } from 'next/router';
+
 function PortfolioProjectPage() {
+  const router = useRouter();
+
   return <div>
     <h1>The Portfolio Project Page</h1>
+    <h3>{router.query.projectid} is the leave</h3>
   </div>
 }
 
