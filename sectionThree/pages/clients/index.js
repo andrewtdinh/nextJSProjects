67efd122afb1afc325eb5b408bc2e3290dd6207e
Andrew Dinh <andrewdinh74@yahoo.com>
2021-08-30 21:44:26 -0700

    Use dynamic links in Link component on Clients page

--- sectionThree/pages/clients/index.js
+++ sectionThree/pages/clients/index.js
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+
 function ClientsPage() {
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        <li><Link href="clients/andrew">Andrew</Link></li>
+        <li><Link href="clients/trieu">Trieu</Link></li>
+      </ul>
     </div>
   )
 }
