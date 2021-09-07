58d3722003d5432de9c98dfa663a02a66a71925a
Andrew Dinh <andrewdinh74@yahoo.com>
2021-09-07 13:03:47 -0700

    Build out the EventItem component

--- sectionFour/components/events/event-item.js
+++ sectionFour/components/events/event-item.js
@@ -1,6 +1,20 @@
 function EventItem(props) {
   return <li>
-    
+    <img src="" alt="" />
+    <div>
+      <div>
+        <h2>TITLE</h2>
+        <div>
+          <time>DATE</time>
+        </div>
+        <div>
+          <address>ADDRESS</address>
+        </div>
+      </div>
+      <div>
+        <Link href="/">Explore Event</Link>
+      </div>
+    </div>
   </li>
 }
 
