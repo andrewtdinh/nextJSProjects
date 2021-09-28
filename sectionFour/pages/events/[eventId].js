575d6dbac2e5b016a5556c440f0fcce2dc11abbc
Andrew Dinh <andrewdinh74@yahoo.com>
2021-09-27 23:49:04 -0700

    Finish building out the event detail

--- sectionFour/pages/events/[eventId].js
+++ sectionFour/pages/events/[eventId].js
@@ -1,6 +1,10 @@
 import { useRouter } from 'next/router';
+import { Fragment } from 'react';
 
 import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
 function EventDetailPage(params) {
   const router = useRouter();
@@ -12,9 +16,18 @@ function EventDetailPage(params) {
     return <p>No event found!</p>
   }
 
-  return <div>
-    <h1>Event Detail</h1>
-  </div>
+  return <Fragment>
+    <EventSummary title={event.title} />
+    <EventLogistics 
+      date={event.date} 
+      address={event.location} 
+      image={event.image} 
+      imageAlt={event.title} 
+    />
+    <EventContent>
+      <p>{event.description}</p>
+    </EventContent>
+  </Fragment>
 }
 
 export default EventDetailPage;
